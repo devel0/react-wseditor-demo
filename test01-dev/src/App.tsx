34f7b2b537c6b48c29e2487a90c3aa640602a7cb
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-10-24 23:47:48 +0200

    test01-dev ( development version of test01 )

---
+++ test01-dev/src/App.tsx
@@ -0,0 +1,138 @@
+import React, { useState, useEffect, useRef } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Grid, TextField, Button, Typography, FormControlLabel, Checkbox, createStyles, Theme, makeStyles } from '@material-ui/core';
+import WSEditorColumn from './react-wseditor/src/WSEditorColumn';
+import WSEditorCellEditorText from './react-wseditor/src/WSEditorCellEditorText';
+import WSEditorCellEditorNumber from './react-wseditor/src/WSEditorCellEditorNumber';
+import WSEditorCellEditorBoolean from './react-wseditor/src/WSEditorCellEditorBoolean';
+import WSEditor from './react-wseditor/src/WSEditor';
+import { WSEditorSelectMode } from './react-wseditor/src/WSEditorSelection';
+
+interface MyData {
+  col1: string,
+  col2: string,
+  col3: number,
+  col4: boolean
+}
+
+const App: React.FC = () => {
+  const [ROWS_COUNT, SET_GRID_SIZE] = useState(12);
+  const [GRID_VIEW_ROWS, SET_GRID_VIEW_ROWS] = useState(6);
+  const [SELECT_MODE_ROWS, SET_SELECT_MODE_ROWS] = useState(false);
+  const [rows, setRows] = useState<MyData[]>([]);
+  const [cols, setCols] = useState<WSEditorColumn<MyData>[]>([]);
+
+  const q1: MyData[] = [];
+
+  useEffect(() => {
+    for (let i = 0; i < ROWS_COUNT; ++i) {
+      q1.push({ col1: 'grp nr ' + Math.trunc(i / 10), col2: 'x' + i, col3: i, col4: true });
+    }
+    setRows(q1);
+    const q2: WSEditorColumn<MyData>[] = [
+      {
+        header: "column 1 (text default)",
+        field: "col1",
+      },
+      {
+        header: "column 2 (text)",
+        field: "col2",
+        editor: (props, editor, viewCell) => new WSEditorCellEditorText(props, editor, viewCell),
+      },
+      {
+        header: "column 3 (number)",
+        field: "col3",
+        editor: (props, editor, viewCell) => new WSEditorCellEditorNumber(props, editor, viewCell),
+      },
+      {
+        header: "column 4 (boolean)",
+        field: "col4",
+        editor: (props, editor, viewCell) => new WSEditorCellEditorBoolean(props, editor, viewCell),
+      },
+    ];
+    setCols(q2);
+  }, [ROWS_COUNT]);
+
+  const useStyles = makeStyles({
+    smallTextField: {
+      width: 50
+    },
+    maginLeft1: {
+      marginLeft: "1em"
+    }
+  });
+
+  const classes = useStyles({});
+  const editorRef = useRef<WSEditor<MyData>>(null);
+
+  return <>
+    <Grid container={true} direction="column">
+      <Grid item={true}>
+        <Grid container={true} direction="row">
+          <Grid item={true} xs="auto">
+            <Typography>Rows count</Typography>
+          </Grid>
+          <Grid item={true} xs="auto" className={classes.maginLeft1}>
+            <TextField className={classes.smallTextField} value={ROWS_COUNT} onChange={(v) => {
+              const n = parseInt(v.target.value);
+              if (!isNaN(n)) SET_GRID_SIZE(parseInt(v.target.value));
+            }} />
+          </Grid>
+          <Grid item={true} xs="auto" className={classes.maginLeft1}>
+            <Typography>Grid view rows</Typography>
+          </Grid>
+          <Grid item={true} xs="auto" className={classes.maginLeft1}>
+            <TextField className={classes.smallTextField} value={GRID_VIEW_ROWS} onChange={(v) => {
+              const n = parseInt(v.target.value);
+              if (!isNaN(n)) SET_GRID_VIEW_ROWS(n);
+            }} />
+          </Grid>
+          <Grid item={true} xs="auto">
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={SELECT_MODE_ROWS}
+                  onChange={(v) => SET_SELECT_MODE_ROWS(v.target.checked)}
+                  value="checkedF"
+                />
+              }
+              label="Select mode rows"
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item={true} xs={12}>
+        <Grid container={true} direction="row">
+          <Button color="primary" onClick={() => {
+            if (editorRef.current) {
+              const editor = editorRef.current;
+              const newRowsCount = editor.props.rows.length + 1;
+              const addedRowIdx = editor.addRow({ col1: "new row", col2: "", col3: 0, col4: false } as MyData, true);
+              editor.scrollToRow(addedRowIdx, newRowsCount);
+              editor.selectRow(addedRowIdx, newRowsCount);
+            }
+          }}>add row</Button>
+          <Button color="primary" onClick={() => {
+            if (editorRef.current) {
+              const editor = editorRef.current;
+              editor.deleteRows(editor.selectedRows());
+            }
+          }}>delete rows</Button>
+        </Grid>
+      </Grid>
+      <Grid item={true} xs={12}>
+        <WSEditor
+          ref={editorRef}
+          rows={rows} setRows={setRows}
+          cols={cols} setCols={setCols}
+          selectionMode={SELECT_MODE_ROWS ? WSEditorSelectMode.Row : WSEditorSelectMode.Cell}
+          viewRowCount={GRID_VIEW_ROWS}
+          outlineCell={false}
+        />
+      </Grid>
+    </Grid>
+  </>
+}
+
+export default App;
