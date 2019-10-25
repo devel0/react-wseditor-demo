66c5440a3d29a4447c05164f15220fb34d9c88dc
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-10-25 17:42:13 +0200

    test02-dev

---
+++ test02-dev/README.md
@@ -0,0 +1,88 @@
+# react-wseditor test01-dev
+
+development test for [react-wseditor](https://github.com/devel0/react-wseditor)
+
+![](doc/sample-debug.png)
+
+this project associate react-wseditor as git submodule in order to allow debug directly tsx files.
+
+## How to contribute (quickstart)
+
+```sh
+git clone https://github.com/devel0/react-wseditor-demo.git
+cd react-wseditor-demo
+cd test01-dev
+git submodule update --init --recursive
+git submodule foreach git pull origin master
+git submodule foreach git checkout master
+yarn install
+code .
+```
+
+from vscode open terminal ctrl+`
+
+```sh
+yarn install
+yarn start
+```
+
+when read hit F5 to debug with chrome ( [more info](https://github.com/devel0/react-wseditor-demo/blob/master/test01-dev/README.md) )
+
+## how this project was built
+
+- create source tree skeleton
+
+```sh
+create-react-app test01-dev --typescript
+cd test01-dev
+yarn add @material-ui/core @material-ui/icons react-icons @types/react-icons
+
+cd src
+git submodule add https://github.com/devel0/react-wseditor
+
+cd ..
+
+cd ..
+```
+
+- setup `tsconfig.json`
+
+```json
+  "exclude":[
+    "src/react-wseditor"
+  ],
+  "include": [
+    "src"
+  ]  
+```
+
+- setup `.env`
+
+```
+BROWSER=none
+```
+
+- setup `.vscode/launch.json`
+
+```json
+{    
+    "version": "0.2.0",
+    "configurations": [                
+        {
+            "type": "chrome",
+            "request": "launch",
+            "name": "Launch Chrome against localhost",
+            "url": "http://localhost:3000",
+            "webRoot": "${workspaceFolder}"            
+        }
+    ]
+}
+```
+
+- debug session
+
+```sh
+yarn start
+```
+
+from vscode hit F5 after yarn started
---
+++ test02-dev/src/App.tsx
@@ -0,0 +1,206 @@
+import React, { useState, useEffect, useRef } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Grid, TextField, Button, Typography, FormControlLabel, Checkbox, createStyles, Theme, makeStyles } from '@material-ui/core';
+import WSEditorColumn from './react-wseditor/src/WSEditorColumn';
+import WSEditorCellEditorText from './react-wseditor/src/WSEditorCellEditorText';
+import WSEditorCellEditorNumber from './react-wseditor/src/WSEditorCellEditorNumber';
+import WSEditorCellEditorBoolean from './react-wseditor/src/WSEditorCellEditorBoolean';
+import WSEditor from './react-wseditor/src/WSEditor';
+import { WSEditorSelectMode } from './react-wseditor/src/WSEditorSelection';
+import useDebounce from './debounce';
+import WSEditorCellEditor from './react-wseditor/src/WSEditorCellEditor';
+
+interface MyData {
+  col1: string,
+  col2: string,
+  col3: number,
+  col4: boolean
+}
+
+const App: React.FC = () => {
+  const [ROWS_COUNT, SET_GRID_SIZE] = useState(1200);
+  const [GRID_VIEW_ROWS, SET_GRID_VIEW_ROWS] = useState(6);
+  const [SELECT_MODE_ROWS, SET_SELECT_MODE_ROWS] = useState(false);
+  const [FILTER, SET_FILTER] = useState("");
+  const [rows, setRows] = useState<MyData[]>([]);
+  const [filteredRows, setFilteredRows] = useState<MyData[]>([]);
+  const [cols, setCols] = useState<WSEditorColumn<MyData>[]>([]);
+
+  const q1: MyData[] = [];
+
+  useEffect(() => {
+    for (let i = 0; i < ROWS_COUNT; ++i) {
+      q1.push({ col1: 'grp nr ' + Math.trunc(i / 10), col2: 'x' + i, col3: i, col4: true });
+    }
+    setRows(q1);
+    setFilteredRows(q1);
+    const q2: WSEditorColumn<MyData>[] = [
+      {
+        header: "R",
+        field: "",
+        editor: (props, editor, viewCell) => new WSEditorCellEditor(props, editor, viewCell, (cellEditor) => {
+          return <>{viewCell.getCellCoord(editor.state.scrollOffset).rowIdx + 1}</>
+        }),
+        minWidth: 400
+      },
+      {
+        header: "column 1 (text default)",        
+        field: "col1",
+        minWidth: 400
+      },
+      {
+        header: "column 2 (text)",
+        field: "col2",
+        editor: (props, editor, viewCell) => new WSEditorCellEditorText(props, editor, viewCell),
+        minWidth: 400
+      },
+      {
+        header: "column 3 (number)",
+        field: "col3",
+        editor: (props, editor, viewCell) => new WSEditorCellEditorNumber(props, editor, viewCell),
+        minWidth: 400
+      },
+      {
+        header: "column 4 (boolean)",
+        field: "col4",
+        editor: (props, editor, viewCell) => new WSEditorCellEditorBoolean(props, editor, viewCell, {
+          label: <Typography style={{ marginRight: "1em" }}>lbl for row idx= {viewCell.getCellCoord(editor.state.scrollOffset).rowIdx}</Typography>,
+          labelPlacement: "start",
+          textAlign: "center"
+        }),
+        minWidth: 400
+      },
+    ];
+    setCols(q2);
+  }, [ROWS_COUNT]);
+
+  const useStyles = makeStyles({
+    smallTextField: {
+      width: 100
+    },
+    maginLeft1: {
+      marginLeft: "1em"
+    }
+  });
+
+  const classes = useStyles({});
+  const editorRef = useRef<WSEditor<MyData>>(null);
+
+  const debouncedFilter = useDebounce(FILTER, 500);
+
+  useEffect(() => {
+    if (debouncedFilter) {
+      const filter = debouncedFilter;
+      if (rows.length > 0) {
+        const keys = Object.keys(rows[0]);
+        const q = rows.filter((x) => {
+          for (let ki = 0; ki < keys.length; ++ki) {
+            const key = keys[ki];
+            const fieldStr = String((x as any)[key]);
+            const qSearch = fieldStr.indexOf(filter);
+            if (qSearch >= 0) return true;
+          }
+          return false;
+        }).slice();
+        console.log(rows.length + " rows filtered to " + q.length);
+        setFilteredRows(q);
+      }
+    }
+  }, [debouncedFilter]);
+
+  return <div
+    style={{
+      position: "absolute",
+      margin: 0, padding: 0,
+      overflow: "auto",
+      width: "100%", height: "100%",
+    }}>
+    <div
+      style={{
+        border: "1px solid", borderStyle: "dashed",
+        margin: "1em", padding: 0,
+        width: "calc(100% - 2em)", height: "calc(100% - 2em)",
+      }}>
+      <Grid container={true} direction="column">
+        <Grid item={true}>
+          <Grid container={true} direction="row">
+            <Grid item={true} xs="auto">
+              <Typography>Rows count</Typography>
+            </Grid>
+            <Grid item={true} xs="auto" className={classes.maginLeft1}>
+              <TextField className={classes.smallTextField} value={ROWS_COUNT} onChange={(v) => {
+                const n = parseInt(v.target.value);
+                if (!isNaN(n)) SET_GRID_SIZE(parseInt(v.target.value));
+              }} />
+            </Grid>
+            <Grid item={true} xs="auto" className={classes.maginLeft1}>
+              <Typography>Grid view rows</Typography>
+            </Grid>
+            <Grid item={true} xs="auto" className={classes.maginLeft1}>
+              <TextField className={classes.smallTextField} value={GRID_VIEW_ROWS} onChange={(v) => {
+                const n = parseInt(v.target.value);
+                if (!isNaN(n)) SET_GRID_VIEW_ROWS(n);
+              }} />
+            </Grid>
+            <Grid item={true} xs="auto" className={classes.maginLeft1}>
+              <Typography>Filter</Typography>
+            </Grid>
+            <Grid item={true} xs="auto" className={classes.maginLeft1}>
+              <TextField className={classes.smallTextField} value={FILTER} onChange={(v) => SET_FILTER(v.target.value)} />
+            </Grid>
+            <Grid item={true} xs="auto">
+              <FormControlLabel
+                className={classes.maginLeft1}
+                control={
+                  <Checkbox
+                    checked={SELECT_MODE_ROWS}
+                    onChange={(v) => SET_SELECT_MODE_ROWS(v.target.checked)}
+                    value="checkedF"
+                  />
+                }
+                label="Select mode rows"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item={true} xs={12}>
+          <Grid container={true} direction="row">
+            <Button color="primary" onClick={() => {
+              if (editorRef.current) {
+                const editor = editorRef.current;
+                const newRowsCount = editor.props.rows.length + 1;
+                const addedRowIdx = editor.addRow({ col1: "new row", col2: "", col3: 0, col4: false } as MyData, true);
+                editor.scrollToRow(addedRowIdx, newRowsCount);
+                editor.selectRow(addedRowIdx, newRowsCount);
+              }
+            }}>add row</Button>
+            <Button color="primary" onClick={() => {
+              if (editorRef.current) {
+                const editor = editorRef.current;
+                editor.deleteRows(editor.selectedRows());
+              }
+            }}>delete rows</Button>
+          </Grid>
+        </Grid>
+        <Grid item={true} xs={12}>
+          <WSEditor
+            ref={editorRef}
+            rows={filteredRows} setRows={setFilteredRows}
+            cols={cols} setCols={setCols}
+            selectionMode={SELECT_MODE_ROWS ? WSEditorSelectMode.Row : WSEditorSelectMode.Cell}
+            selectionModeMulti={false}
+            debug={true}
+            viewRowCount={GRID_VIEW_ROWS}
+            onCellDataChanged={(row, cell, data) => {
+              const q = row.col1; // typed row
+              console.log("data changed on row:" + JSON.stringify(row) + " cell:" + cell + " data:" + data);
+            }}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  </div>
+}
+
+export default App;
